097f508adc8e16f87962728efe0a0ad181e2edc5
Yqc <eraimm@gmail.com>
2023-08-18 18:05:24 +0800

    feat: init

---
+++ src/app/test/page.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: Y
+ * @Date: 2022-09-28
+ * @Description:
+ */
+
+// import { useRef } from 'react'
+// import { BoxGeometry, MeshBasicMaterial, PerspectiveCamera, Scene } from 'three'
+
+const Test = () => {
+  const a = {};
+  // const b = {}
+  return (
+    <div>
+      Tree{a?.a}
+    </div>
+  )
+}
+
+export default Test
